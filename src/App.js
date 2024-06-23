ca3f3cd37f3e2be7fa1549345342bc349577f323
adiucas <adi@ucabd.com>
2024-06-23 11:27:17 +0600

    gfrwegreg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [names, setNames] = useState([
+        { id: '1', name: 'adi' },
+        { id: '2', name: 'joy' },
+        { id: '3', name: 'tutul' }
+    ]);
+
+    return (
+        <>
+            <ul>
+                {names.map((name) => (
+                    <li key={name.id}>{name.id} {name.name}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default App;
